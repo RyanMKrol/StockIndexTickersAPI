1f9679681a017981ea3823e456b4501dd1dca7eb
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-23 14:35:08 +0000

    Plug in tickers data to api module
    * Code copied from old implementation

--- src/routes/tickers.js
+++ src/routes/tickers.js
@@ -4,7 +4,8 @@ import { fetchTickers } from './../api/tickers'
 const router = express.Router()
 
 router.get('/:index', async (req, res, next) => {
-  res.send(fetchTickers())
+  const tickers = await fetchTickers(req.params.index)
+  res.send(tickers)
 })
 
 export default router
