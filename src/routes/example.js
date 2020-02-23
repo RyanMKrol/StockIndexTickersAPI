98d42edcb5a832cc0d4d63edd39378c494a8ec19
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-23 14:11:51 +0000

    Update deployment scripts

--- src/routes/example.js
+++ src/routes/example.js
@@ -1,11 +1,6 @@
 import express from 'express'
 
-var router = express.Router()
-
-router.use('/', async (req, res, next) => {
-  console.log("Using some custom middleware!")
-  next()
-})
+const router = express.Router()
 
 router.get('/', async (req, res, next) => {
   res.send("Sending back some data")
